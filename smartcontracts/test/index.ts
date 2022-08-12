b31b02d7e563823d09e248a85031b14f5a1b8a2d
CarrotIfson <didacysusana@gmail.com>
2022-08-12 17:24:06 +0200

    Added UNHAPPY path tests

--- smartcontracts/test/index.ts
+++ smartcontracts/test/index.ts
@@ -12,7 +12,7 @@ chai.use(chaiAsPromised);
 function parseEther(amount: Number) {
     return ethers.utils.parseUnits(amount.toString(), 18);
 }
-
+ 
 
 describe("Vault", function () {
     let owner:SignerWithAddress,
@@ -34,6 +34,8 @@ describe("Vault", function () {
         await vault.setToken(token.address);
     })
     
+
+    ////// HAPPY PATH  /////////////////////////
     it("Should deposit into the vault", async() => { 
         await token.transfer(alice.address, parseEther(1 * 10**6)); 
         await token.connect(alice).approve(vault.address, token.balanceOf(alice.address)); 
@@ -59,5 +61,61 @@ describe("Vault", function () {
         expect(await token.balanceOf(vault.address)).equal(parseEther(200*10**3)); 
         expect(await token.balanceOf(bob.address)).equal(parseEther(300*10**3));
     });
+
+
+    ////// UNHAPPY PATH  ///////////////////////
+    it("Should FAIL to deposit, Insufficient account balance", async() => { 
+        await token.transfer(alice.address, parseEther(1 * 10**6)); 
+        await token.connect(alice).approve(vault.address, token.balanceOf(alice.address));  
+        await expect(vault.connect(alice).deposit(parseEther(2*10**6))).revertedWith("Insufficient account balance"); 
+        //set approve to balance-1
+        await token.connect(alice).approve(vault.address, (await token.balanceOf(alice.address)).sub(1));  
+        await expect(vault.connect(alice).deposit(parseEther(1*10**6))).revertedWith("ERC20: insufficient allowance");  
+    });
+
+    it("Should FAIL to withdraw, Caller is not a withdrawer", async() => {  
+        await token.transfer(alice.address, parseEther(1 * 10**6)); 
+        await token.connect(alice).approve(vault.address, token.balanceOf(alice.address));  
+        await vault.connect(alice).deposit(parseEther(1*10**6));
+        await expect(vault.connect(alice).withdraw(parseEther(1*10*3), alice.address)).revertedWith("Caller is not a withdrawer");    
+    });
+
+    it("Should FAIL to withdraw, Withdraw is not enabled", async() => {  
+        //grant withdrawer role to alice
+        let WITHDRAWER_ROLE = keccak256(Buffer.from("WITHDRAWER_ROLE")).toString();
+        await vault.grantRole(WITHDRAWER_ROLE, alice.address); 
+
+        await token.transfer(alice.address, parseEther(1 * 10**6)); 
+        await token.connect(alice).approve(vault.address, token.balanceOf(alice.address));  
+        await vault.connect(alice).deposit(parseEther(1*10**6));
+        await expect(vault.connect(alice).withdraw(parseEther(1*10*3), alice.address)).revertedWith("Withdraw is not enabled");     
+
+        await vault.setWithdrawEnabled(true);
+        await vault.connect(alice).withdraw(parseEther(0), alice.address);
+ 
+        await vault.setWithdrawEnabled(false);
+        await expect(vault.connect(alice).withdraw(parseEther(1*10*3), alice.address)).revertedWith("Withdraw is not enabled");  
+    });
+
+    it("Should FAIL to withdraw, Amount should be lower than maxWithdrawalAmount", async() => {  
+        //grant withdrawer role to alice
+        let WITHDRAWER_ROLE = keccak256(Buffer.from("WITHDRAWER_ROLE")).toString();
+        await vault.grantRole(WITHDRAWER_ROLE, alice.address); 
+        await vault.setWithdrawEnabled(true);
+
+        await token.transfer(alice.address, parseEther(1 * 10**6)); 
+        await token.connect(alice).approve(vault.address, token.balanceOf(alice.address));  
+        await vault.connect(alice).deposit(parseEther(1*10**6));
+        await expect(vault.connect(alice).withdraw(parseEther(1*10*3), alice.address)).revertedWith("Amount should be lower than maxWithdrawalAmount");   
+
+        await vault.setMaxWithdrawalAmount(parseEther(10));
+        await vault.connect(alice).withdraw(parseEther(10), alice.address);
+
+        await vault.setMaxWithdrawalAmount(parseEther(0));
+        await expect(vault.connect(alice).withdraw(parseEther(1*10*3), alice.address)).revertedWith("Amount should be lower than maxWithdrawalAmount");     
+    });
+
+
+
     
 });
